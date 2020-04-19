0c34ea4a7a81db0530d27499461980de10e4c13b
Lorenzo Angelini <lorenzoangelini@MBPdiLorenzo2.fritz.box>
2020-04-19 12:56:37 +0200

    init(noiapp): init - add android DP3T module

---
+++ noiapp/android/app/src/main/java/com/noiapp/DP3T/DP3TModule.java
@@ -0,0 +1,35 @@
+package com.noiapp.DP3T;
+import androidx.annotation.NonNull;
+
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+
+import org.dpppt.android.sdk.DP3T;
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class DP3TModule extends ReactContextBaseJavaModule {
+
+    final String MODULE_NAME = "DP3TModule";
+    private static ReactApplicationContext reactContext;
+
+    @NonNull
+    @Override
+    public String getName() {
+        return MODULE_NAME;
+    }
+
+    DP3TModule(ReactApplicationContext context){
+        super(context);
+        reactContext = context;
+    }
+
+    @ReactMethod
+    public void init(String message) {
+        DP3T.init(reactContext, "com.noiapp");
+    }
+}
